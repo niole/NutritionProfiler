6f2ac2acbf9c9a3a1e6ee209fe917672fd2d61a0
Niole Nelson <niolenelson@gmail.com>
2017-01-02 14:32:14 -0800

    can get links and text

--- nutritionalProfiler.go
+++ nutritionalProfiler.go
@@ -3,22 +3,34 @@ package main
 import (
     "fmt"
     "net/http"
+    "strings"
     "io/ioutil"
-    "regexp"
+    "golang.org/x/net/html"
 )
-
-var linkPattern = rgx(`<a href="(.+?)"`)
-
-func rgx(pattern string) *regexp.Regexp {
-  //returns regexp version of pattern
-  return regexp.MustCompile(pattern)
-}
 
 type Callback func()
 
 type Queue struct {
     elements []string
 }
+
+func (q * Queue) isEmpty() (empty bool) {
+    empty = len(q.elements) == 0
+    return
+}
+
+func (q * Queue) pop() (nextElement string) {
+    nextElement = q.elements[0]
+
+    q.elements = q.elements[1:]
+
+    return
+}
+
+func (q * Queue) push(newElement string) {
+    q.elements = append(q.elements, newElement)
+}
+
 
 type Map struct {
     elements map[string]string
@@ -40,40 +52,30 @@ func initMap(fields []string) map[string]string {
     return elements
 }
 
-func (q * Queue) isEmpty() (empty bool) {
-    empty = len(q.elements) == 0
-    return
-}
-
-func (q * Queue) pop() (nextElement string) {
-    nextElement = q.elements[0]
-
-    q.elements = q.elements[1:]
-
-    return
-}
-
-func (q * Queue) push(newElement string) {
-    q.elements = append(q.elements, newElement)
-}
 
 type Crawler struct {
     keyWords []string
     links Queue
-    getNextLinks func([]byte)[][][]byte
     seen map[string]bool
+    text [][]string
 }
 
 func (c * Crawler) getPages() {
     for !c.links.isEmpty() {
 
-        fmt.Println(len(c.links.elements))
-
         nextPage := c.next()
-        nextLinks := c.getNextLinks(nextPage)
 
-        for _, link := range nextLinks {
-            c.links.push(string(link[1]))
+        data := getTextFromPage(nextPage)
+
+        if (len(data["text"]) > 0) {
+          fmt.Println(data["text"])
+          c.text = append(c.text, data["text"])
+        }
+
+        nextLinks := data["link"]
+
+        for _, l := range nextLinks {
+          c.links.push(l)
         }
     }
 }
@@ -82,12 +84,7 @@ func (c * Crawler) done(page string) (isDone bool) {
     return
 }
 
-func getNextLinks(page []byte) (nextLinks [][][]byte) {
-    nextLinks = linkPattern.FindAllSubmatch(page, -1)
-    return
-}
-
-func (c * Crawler) next() (nextPage []byte) {
+func (c * Crawler) next() (nextPage string) {
     nextLink := c.links.pop()
 
     if (!c.seen[nextLink]) {
@@ -98,9 +95,10 @@ func (c * Crawler) next() (nextPage []byte) {
         defer resp.Body.Close()
         body, err := ioutil.ReadAll(resp.Body)
         errHandler(err, func() {
-          nextPage = body
+          nextPage = string(body)
         })
       })
+
     }
 
     return
@@ -109,7 +107,6 @@ func (c * Crawler) next() (nextPage []byte) {
 type NutritionProfile struct {
     profile Map
 }
-
 
 func errHandler(err error, callback Callback) {
   if (err != nil) {
@@ -120,12 +117,46 @@ func errHandler(err error, callback Callback) {
   }
 }
 
+func getTextFromPage(page string) (pageData map[string][]string) {
+  doc, err := html.Parse(strings.NewReader(page))
+
+  if (err == nil) {
+    pageData = inspectParsedHTML(doc, make(map[string][]string))
+  }
+
+  return
+}
+
+func inspectParsedHTML(n *html.Node, pageData map[string][]string) map[string][]string {
+
+    if n.Type == html.ElementNode && n.Data == "a" {
+        for _, a := range n.Attr {
+            if (a.Key == "href") {
+              pageData["link"] = append(pageData["link"], a.Val)
+            }
+        }
+    }
+
+    if (n.Type == html.TextNode) {
+      text := n.Data
+      pageData["text"] = append(pageData["text"], text)
+    }
+
+
+    for c := n.FirstChild; c != nil; c = c.NextSibling {
+        inspectParsedHTML(c, pageData)
+    }
+
+    return pageData
+}
+
 func main() {
     start := "http://www.livestrong.com/"
     keyWords := []string{"health", "food", "diet", "superfood", "fat", "carbohydrates", "protein"}
     seen := make(map[string]bool)
     links := Queue{[]string{start}}
-    crawler := Crawler{keyWords, links, getNextLinks, seen}
+    text := make([][]string, 0)
+    crawler := Crawler{keyWords, links, seen, text}
 
     crawler.getPages()
 
